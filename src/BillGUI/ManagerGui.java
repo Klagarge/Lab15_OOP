0229035d6928a18a5d0cac2be30429b21fd6734f
Rémi Heredero <remi@heredero.ch>
2022-05-15 19:06:32 +0200

    insert grid layout

--- src/BillGUI/ManagerGui.java
+++ src/BillGUI/ManagerGui.java
@@ -1,10 +1,13 @@
 package BillGUI;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  * 
  */
 public class ManagerGui extends JFrame{
-    
+    GridLayout grid = new GridLayout(2,9);
+
 }
