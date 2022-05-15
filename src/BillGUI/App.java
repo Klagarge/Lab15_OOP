0a4579215b75ebd6a5368cc1d4445b1348c1d21d
Rémi Heredero <remi@heredero.ch>
2022-05-15 20:44:07 +0200

    just started

---
+++ src/BillGUI/App.java
@@ -0,0 +1,11 @@
+package BillGUI;
+
+public class App {
+    public static void main(String[] args) {
+        int rows = 9;
+        int cols = 2;
+        String name;
+        String logoFilePath;
+        new ManagerGui(rows, cols, name, logoFilePath);
+    }
+}
--- src/BillGUI/ManagerGui.java
+++ src/BillGUI/ManagerGui.java
@@ -8,6 +8,16 @@ import javax.swing.*;
  * 
  */
 public class ManagerGui extends JFrame{
-    GridLayout grid = new GridLayout(2,9);
+    GridLayout grid;
+    JLabel Jname;
+    JLabel Jlogo;
+    
+
+    public ManagerGui(int n,int m, String name, String logoFilePath){
+        grid = new GridLayout(n,m);
+        Jname = new JLabel(name);
+    }
+
+    
 
 }
