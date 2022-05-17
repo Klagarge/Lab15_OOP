665a5e2292ac0f1850c3a377da2ef234d88eca7d
Rémi Heredero <remi@heredero.ch>
2022-05-17 19:22:49 +0200

    push for commun work

--- src/BillGUI/App.java
+++ src/BillGUI/App.java
@@ -4,8 +4,8 @@ public class App {
     public static void main(String[] args) {
         int rows = 9;
         int cols = 2;
-        String name;
-        String logoFilePath;
+        String name = "Garage manager \n Prestations";
+        String logoFilePath = "src/logo_garage.png";
         new ManagerGui(rows, cols, name, logoFilePath);
     }
 }
