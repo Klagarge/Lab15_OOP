e245dcba5224a0dfc6b3242e1d6f859c3cd8e615
Fastium <79766552+Fastium@users.noreply.github.com>
2022-05-19 12:35:53 +0200

    comment in hessoGarage and biullGui

--- src/BillGUI/HesSoGarage.java
+++ src/BillGUI/HesSoGarage.java
@@ -2,9 +2,11 @@ package BillGUI;
 
 public class HesSoGarage {
     public static void main(String[] args) {
+        //Create a new garage
         GarageManager garageManager = new GarageManager();
         String name = "<html><body><i>Garage manager</i><br>Prestations</body></html>";
         String logoFilePath = "src/logo_garage.png"; //TODO redimensionnement automatique
+        //Create a new HMI for calculate and print the bill
         new ManagerGui(name, logoFilePath, garageManager.getServices(), garageManager); //TODO affichage sans redimensionnement
     }
 }
