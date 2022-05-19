470044ff13f81a75c8fb261964b479b9f5708293
Rémi Heredero <remi@heredero.ch>
2022-05-19 09:10:46 +0200

    only ugly full screen

--- src/BillGUI/HesSoGarage.java
+++ src/BillGUI/HesSoGarage.java
@@ -3,10 +3,9 @@ package BillGUI;
 public class HesSoGarage {
     public static void main(String[] args) {
         GarageManager garageManager = new GarageManager();
-        ManagerGui managerGui;
         String name = "\033[3mGarage manager\033[0m\nPrestations"; //TODO résoudre en italique
         String logoFilePath = "src/logo_garage.png"; //TODO redimensionnement automatique
-        managerGui = new ManagerGui(name, logoFilePath, garageManager.getServices(), garageManager); //TODO affichage sans redimmensionnement
+        new ManagerGui(name, logoFilePath, garageManager.getServices(), garageManager); //TODO affichage sans redimensionnement
     }
 }
 
